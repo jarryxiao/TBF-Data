febe7a0b2615b54218ff216efcc8d55a4e752672
Jarry Xiao <jarry.xiao@berkeley.edu>
2016-08-28 21:25:00 -0700

    finished user matrix

--- eventrec.py
+++ eventrec.py
@@ -10,6 +10,7 @@ event_index = 0
 """ State of individuals. """
 email_to_index = {}
 index_to_email = {}
+emails_to_events = {}
 email_index = 0
 
 """ State of the matrix array. """
@@ -20,48 +21,57 @@ attendance = []
 """ Opens eventdata.csv and populates event_to_index and index_to_event. """
 with open('eventdata.csv') as csvfile:
     sheet = csv.reader(csvfile, delimiter= ',', quotechar='|')
+    next(sheet)
     for row in sheet:
-        attendance.append([str(row[18], str(row[0]), str(row[6]).lower())])
+
+        status = row[18]
+        if status == "Attending":
+            status = 0.5
+        else:
+            status = 1
+
         event = str(row[0])
         email = str(row[6]).lower()
         if event not in event_to_index.keys():
             event_to_index[event] = event_index
             index_to_event[event_index] = event
             event_index += 1
-        if email not in email_to_index.keys() and '@' in email:
+
+        current_event = (event_to_index[event], status)
+        if email not in email_to_index.keys():
             email_to_index[email] = email_index
             index_to_email[email_index] = email
+            emails_to_events[email] = []
             email_index += 1
+        try:
+            emails_to_events[email].append(current_event)
+        except:
+            print(email)
     """ Creates an array of zeros the size of the number of events. """
-    i = 0
-    zero_list = []
-    while i < event_index + 1: # event_index + 1 == number of events
-        zero_list.append(0)
-        i += 1
+    print(emails_to_events)
+
+
     """ Creates an array of arrays of zeros the size of the number of email * the number of events. """
     j = 0
     matrix_array = []
-    while j < email_index + 1:
-        matrix_array.append(zero_list)
+    while j < email_index:
+        matrix_array.append([0 for i in range(event_index)])
         j += 1
+    print(matrix_array)
     """ Fills in .5 for an email that signed up for a ticket and 1 for an email that checked in. """
-    sheet = csv.reader(csvfile, delimiter= ',', quotechar='|')
-    for row in attendance:
-        if str(row[0]) == "Attending":
-            email_position = row[2]
-            event_position = row[1]
-            matrix_array[email_position][event_position] = .5
-        if str(row) == "Checked In":
-            email_position = row[2]
-            event_position = row[1]
-            matrix_array[email_position][event_position] = 1
+    for email in emails_to_events.keys():
+        row = email_to_index[email]
+        for col, val in emails_to_events[email]:
+
+            matrix_array[row][col] = val
 
 """ Creates a matrix where each row is an implicit user and each column is an implicit event. """
-user_matrix = np.matrix(matrix_array)
-print(user_matrix)
+user_matrix = np.array(matrix_array)
+g = "smccall@berkeley.edu"
+print(user_matrix[email_to_index[g]])
 
 def S(i, j):
-    return np.linalg.norm(UM[i] - UM[j])
+    return np.linalg.norm(user_matrix[i] - user_matrix[j])
 
 def kNN(k, guest):
     guestindex = guesttoindex[guest]
@@ -69,7 +79,7 @@ def kNN(k, guest):
     for i in range(len(UM)):
         if i != guestindex:
             simularity = -S(guestindex, i)
-            if !neighbors.full():
+            if not neighbors.full():
                 neighbors.get()
             neighbors.put((simularity, indextoguest[i]))
 
