2513f47893cb8c92b2e53bcbcc38054546b76f2d
Simon Behar <simbeh7@gmail.com>
2016-09-18 22:00:08 -0700

    added directory crawler

---
+++ directory_crawler.py
@@ -0,0 +1,29 @@
+import csv
+import urllib.request
+import re
+
+def collect():
+    data = None
+    with open('data.csv') as csvdata:
+        d = csv.reader(csvdata)
+        data = list(d)
+    with open('new_data.csv', 'w') as newdata:
+        writer = csv.writer(newdata, delimiter=',')
+        writer.writerow(["Email", "UID"])
+        for row in data[1:]:
+            email = row[6]
+            if email.split('@')[1] == "berkeley.edu":
+                try:
+                    uid = get_uid(email.split('@')[0])
+                except:
+                    print("Connection error:", count)
+                if uid:
+                    writer.writerow([email, uid])
+
+def get_uid(email):
+    url = "https://calnet-p2.calnet.berkeley.edu/directory/search.pl?search-type=email&search-base=student&search-term=" + email + "%40berkeley.edu&search=Search"
+    res = urllib.request.urlopen(url).read().decode("utf8")
+    m = re.findall(r'uid=([0-9]+)', res)
+    return m[0] if m and len(m) == 1 else 0
+
+collect()
